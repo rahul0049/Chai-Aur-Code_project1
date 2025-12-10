840519cb68e606a57bb5c65ecb9b3e69c225d8cf
Rahul <rahulrajoriya089@gmail.com>
2025-12-11 02:18:52 +0530

    user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistroy, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 const router=Router()
 import { loginUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import multer from "multer";
+
+
 
 router.route("/register").post(
     upload.fields([
@@ -19,5 +22,12 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)//here verifyJWT is a middleware and next() in middleware told to go to logoutUser
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword) // middleware used so that only verified user can access
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-details").patch(verifyJWT,updateAccountDetails) // if we use post then all details get updates so use patch
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile) // this is because we are getting data from params so use c/:username
+router.route("/history").get(verifyJWT,getWatchHistroy)
 
 export default router
